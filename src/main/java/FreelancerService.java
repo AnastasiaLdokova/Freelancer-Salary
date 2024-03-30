7828a90b9c0a9bef7204f77b83e7ba751b84c1fa
Anastasia.L <easy.club.info@gmail.com>
2024-03-30 13:00:42 +0300

    reformate code

--- src/main/java/FreelancerService.java
+++ src/main/java/FreelancerService.java
@@ -1,17 +1,17 @@
 public class FreelancerService {
-    public int calculate(int income,int expences, int threshold) {
+    public int calculate(int income, int expences, int threshold) {
         int money = 0;
-        int counter=0;
+        int counter = 0;
         for (int i = 0; i < 12; i++) {
             if (money >= threshold) {
-                money-=expences;
-                money/= 3;
+                money -= expences;
+                money /= 3;
                 counter++;
 
-            }else{
-               //money = money + income;
-               money += income;
-               money -=expences;
+            } else {
+                //money = money + income;
+                money += income;
+                money -= expences;
             }
 
         }
--- src/test/java/FreelancerServiceTest.java
+++ src/test/java/FreelancerServiceTest.java
@@ -2,13 +2,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-
 class FreelancerServiceTest {
     @Test
     public void testCase1() {
-        int income=10_000;
-                int expences=3_000;
-                int threshold=20_000;
+        int income = 10_000;
+        int expences = 3_000;
+        int threshold = 20_000;
         FreelancerService service = new FreelancerService();
         int actual = service.calculate(income, expences, threshold);
 
@@ -19,9 +18,9 @@ class FreelancerServiceTest {
 
     @Test
     public void testCase2() {
-        int income=100_000;
-        int expences=60_000;
-        int threshold=150_000;
+        int income = 100_000;
+        int expences = 60_000;
+        int threshold = 150_000;
         FreelancerService service = new FreelancerService();
         int actual = service.calculate(income, expences, threshold);
         int expected = 2;
